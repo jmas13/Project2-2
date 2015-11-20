71b041f0e458cf8a31e88151697233e8c79859ee
Negar Kalbasi <neka@Negars-MacBook-Pro.local>
2015-11-20 10:32:07 -0500

    s3 troubleshooting

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,28 +1,29 @@
-# CarrierWave.configure do |config|
-#   config.fog_credentials = {
-#       :provider               => 'AWS',
-#       :aws_access_key_id      => ENV['aws_access_key_id'],
-#       :aws_secret_access_key  => ENV['aws_secret_access_key'],
-#       :region                 => "us-west-2"
-#   }
-#   config.fog_directory  = ENV['aws_bucket']
-# end
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider              => 'AWS',
-    :aws_access_key_id     => ENV['aws_access_key_id'],
-    :aws_secret_access_key => ENV['aws_secret_access_key']
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['aws_access_key_id'],
+      :aws_secret_access_key  => ENV['aws_secret_access_key'],
+      :region                 => "us-west-2"
   }
-
-  if Rails.env.test? || Rails.env.cucumber?
-    config.storage = :file
-    config.enable_processing = false
-    config.root = "#{Rails.root}/tmp"
-  else
-    config.storage = :fog
-  end
-
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
-
-  config.fog_directory = ENV['aws_bucket']
+  config.fog_directory  = ENV['aws_bucket']
 end
+# CarrierWave.configure do |config|
+#   config.fog_credentials = {
+#     :provider              => 'AWS',
+#     :aws_access_key_id     => ENV['aws_access_key_id'],
+#     :aws_secret_access_key => ENV['aws_secret_access_key']
+#     :region                 => "us-west-2"
+#   }
+#
+#   if Rails.env.test? || Rails.env.cucumber?
+#     config.storage = :file
+#     config.enable_processing = false
+#     config.root = "#{Rails.root}/tmp"
+#   else
+#     config.storage = :fog
+#   end
+#
+#   config.cache_dir = "#{Rails.root}/tmp/uploads"
+#
+#   config.fog_directory = ENV['aws_bucket']
+# end
